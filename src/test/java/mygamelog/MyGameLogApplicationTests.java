47f39e7fe9cde2a59e961cf32b0a4cbfe1b22be8
IsmaelRuizJurado <ismruijur@alum.us.es>
2023-05-31 11:50:12 +0200

    First commit :)

---
+++ src/test/java/mygamelog/MyGameLogApplicationTests.java
@@ -0,0 +1,13 @@
+package mygamelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
